82a6f82022fc9c87138c787e046db4c73b03e7f3
Luis Rubio <luirting@gmail.com>
2020-06-16 23:41:47 -0700

    Initial commit with config

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-base
+
+React base project to initialize a react project with default webpack configuration using eslint, prettier and babel.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const javascriptRules = {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-react', '@babel/preset-env'],
+    },
+  },
+};
+
+const htmlRules = {
+  test: /\.html$/,
+  use: {
+    loader: 'html-loader',
+  },
+};
+
+const cssRules = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+};
+
+module.exports = (env, { mode }) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [javascriptRules, htmlRules, cssRules],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+  };
+};
